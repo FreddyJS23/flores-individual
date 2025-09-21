d0d20d49ed87814dfda8d6d244c1c5ccc6ddf80d
Freddy Solarte <freddyjsolarte@gmail.com>
2025-09-21 16:12:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ const finalModal = document.getElementById("finalModal");
 const closeFinal = document.getElementById("closeFinal");
 const instructions = document.getElementById("instructions");
 
+const queryString = window.location.search;
+
+const parametro = new URLSearchParams(queryString);
+
+const nombre = parametro.get("n"); 
+
+function primeraLetraEnMayusculas(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 const MAX_FLOWERS = 10;
 const MAX_PARTICLES = 36;
 let activeParticles = 0;
@@ -19,7 +29,7 @@ let typingTimer = null;
 
 /* Mensaje (ahora más lento y con pausas más largas) */
 const messageLines = [
-  `Audimar, no te traje flores amarillas.
+  `${nombre ? primeraLetraEnMayusculas(nombre) :  'Cariño'}, no te traje flores amarillas.
   No porque no las valore, sino porque se que al final se marchitan y dejan un espacio vacío en el florero.`,
 
   `Lo que sostienes ahora no busca reemplazar nada, sino recordarte algo: lo verdadero no se mide en colores,
